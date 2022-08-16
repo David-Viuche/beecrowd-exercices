e69e53ffd5a624abf09f094bd8c13ebb0d2149e0
David Viuche <jviucheroja@uniminuto.edu.co>
2022-08-15 21:33:58 -0500

    1010 solution accepted - 3

--- src/problems/beginner/1010.js
+++ src/problems/beginner/1010.js
@@ -3,9 +3,10 @@ let lines = input.split('\n');
 
 let totalSale = 0;
 
-lines.map(el => {
-    let product = el.split(' ');
-    totalSale += parseFloat(product[1]) * parseFloat(product[2]);
-});
+let product = lines.shift().split(' ');
+totalSale += parseFloat(product[1]) * parseFloat(product[2]);
+
+product = lines.shift().split(' ');
+totalSale += parseFloat(product[1]) * parseFloat(product[2]);
 
 console.log("VALOR A PAGAR: R$ " + totalSale.toFixed(2));
