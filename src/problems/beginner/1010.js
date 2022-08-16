05b358675c228017c91ac75dec08f6fa4797b381
DESKTOP-1UU1HA8\David <viuchejuan@gmail.com>
2022-08-15 21:17:56 -0500

    send 1 1010 wrong answer

---
+++ src/problems/beginner/1010.js
@@ -0,0 +1,11 @@
+let input = require('fs').readFileSync('/dev/stdin', 'utf8');
+let lines = input.split('\n');
+
+let totalSale = 0;
+
+lines.map(el => {
+    let product = el.split(' ');
+    totalSale += parseFloat(product[1]) * parseFloat(product[2]);
+});
+
+console.log("VALOR A PAGAR: R$ " + totalSale);
