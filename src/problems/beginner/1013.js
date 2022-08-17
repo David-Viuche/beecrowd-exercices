606beec50c2f62a1e7b5d1b8b115497567930d10
David Viuche <jviucheroja@uniminuto.edu.co>
2022-08-16 22:27:11 -0500

    1013 test case implementation

--- src/problems/beginner/1013.js
+++ src/problems/beginner/1013.js
@@ -1,16 +1,19 @@
-let input = require('fs').readFileSync('/dev/stdin', 'utf8');
-let lines = input.split('\n');
-
-let data = lines.shift().split(' ');
-
-let a = parseInt(data[0]);
-let b = parseInt(data[1]);
-let c = parseInt(data[2]);
-
 function higherAB(a, b) {
     return (a + b + Math.abs(a - b)) / 2;
 }
 
-let aux = higherAB(a, b);
+function _1013_(lines) {
+    let data = lines.shift().split(' ');
 
-console.log(higherAB(aux, c) + " eh o maior");
+    let a = parseInt(data[0]);
+    let b = parseInt(data[1]);
+    let c = parseInt(data[2]);
+
+    let aux = higherAB(a, b);
+
+    return higherAB(aux, c) + " eh o maior";
+}
+
+//console.log(_1013_(lines));
+
+module.exports = _1013_;
---
+++ src/test/beginner/1013.test.js
@@ -0,0 +1,9 @@
+const _1013_ = require('../../problems/beginner/1013');
+
+test('1013 case 1 ', ()=>{
+    expect(_1013_(['7 14 106'])).toBe('106 eh o maior')
+});
+
+test('1013 case 2 ', ()=>{
+    expect(_1013_(['217 14 6'])).toBe('217 eh o maior')
+});
