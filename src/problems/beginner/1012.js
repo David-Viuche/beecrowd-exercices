58591c810e55c139fce60d23130b3ed95de0d650
David Viuche <jviucheroja@uniminuto.edu.co>
2022-08-16 20:39:03 -0500

    1012-3 Accepted

--- src/problems/beginner/1012.js
+++ src/problems/beginner/1012.js
@@ -7,7 +7,7 @@ let a = parseFloat(data[0]);
 let b = parseFloat(data[1]);
 let c = parseFloat(data[2]);
 
-console.log("TRIANGULO: " + ((a * b) / 2).toFixed(3));
+console.log("TRIANGULO: " + ((a * c) / 2).toFixed(3));
 console.log("CIRCULO: " + (3.14159 * Math.pow(c, 2)).toFixed(3));
 console.log("TRAPEZIO: " + (((a + b) * c) / 2).toFixed(3));
 console.log("QUADRADO: " + (b * b).toFixed(3));
