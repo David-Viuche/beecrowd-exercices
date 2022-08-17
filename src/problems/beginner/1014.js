c04e547d8c9d945394280c0061aad2740fd8579e
David Viuche <jviucheroja@uniminuto.edu.co>
2022-08-16 22:30:16 -0500

    1014 test case implementation

--- src/problems/beginner/1014.js
+++ src/problems/beginner/1014.js
@@ -1,7 +1,10 @@
-let input = require('fs').readFileSync('/dev/stdin', 'utf8');
-let lines = input.split('\n');
+function _1014_(lines) {
+    let kmTra = parseInt(lines[0]);
+    let consum = parseFloat(lines[1]);
 
-let kmTra = parseInt(lines[0]);
-let consum = parseFloat(lines[1]);
+    return (kmTra / consum).toFixed(3) + ' km/l';
+}
 
-console.log((kmTra / consum).toFixed(3) + ' km/l');
+//console.log(_1014_(lines));
+
+module.exports = _1014_;
