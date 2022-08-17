13fdc56015434b8102ef666665cf7884fbd85b81
David Viuche <jviucheroja@uniminuto.edu.co>
2022-08-16 20:34:35 -0500

    1012-2 Wrong answer (100%)

--- src/problems/beginner/1012.js
+++ src/problems/beginner/1012.js
@@ -7,10 +7,8 @@ let a = parseFloat(data[0]);
 let b = parseFloat(data[1]);
 let c = parseFloat(data[2]);
 
-console.log(
-    "TRIANGULO: " + ((a * b) / 2).toFixed(3) + "\n" +
-    "CIRCULO: " + (3.14159 * Math.pow(c, 2)).toFixed(3) + "\n" +
-    "TRAPEZIO: " + (((a + b) * c) / 2).toFixed(3) + "\n" +
-    "QUADRADO: " + (b * b).toFixed(3) + "\n" +
-    "RETANGULO: " + (a * b).toFixed(3)
-);
+console.log("TRIANGULO: " + ((a * b) / 2).toFixed(3));
+console.log("CIRCULO: " + (3.14159 * Math.pow(c, 2)).toFixed(3));
+console.log("TRAPEZIO: " + (((a + b) * c) / 2).toFixed(3));
+console.log("QUADRADO: " + (b * b).toFixed(3));
+console.log("RETANGULO: " + (a * b).toFixed(3));
