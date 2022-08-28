2fad7eae4d00c7668950c55a0314db8ab804ce1c
David Viuche <jviucheroja@uniminuto.edu.co>
2022-08-27 21:33:26 -0500

    added init command

---
+++ src/utils/init.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+
+let code = process.argv[2];
+
+let contentJs = `\nfunction _${code}_(lines) {\n\n\treturn\n}\n\n//console.log(_${code}_(lines));\n\nmodule.exports = _${code}_;`;
+let contentTest = `\nconst _${code}_ = require('../../problems/beginner/${code}');\n\ntest('${code} case 1 ', ()=>{\n    expect(_${code}_([''])).toBe('')\n});\n\ntest('${code} case 2 ', ()=>{\n    expect(_${code}_([''])).toBe('')\n});\n\ntest('${code} case 3 ', ()=>{\n    expect(_${code}_([''])).toBe('')\n});`;
+
+try {
+    console.log("Init problem... " + code);
+    fs.writeFileSync(`./src/problems/beginner/${code}.js`, contentJs);
+    fs.writeFileSync(`./src/test/beginner/${code}.test.js`, contentTest);
+    console.log("Init successful... :) " + code);
+} catch (e) {
+    console.log("Error init :c - ", e);
+}
