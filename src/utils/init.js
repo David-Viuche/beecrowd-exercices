041c3b6f5e0300628de27b7003990ec6e922d3af
David Viuche <jviucheroja@uniminuto.edu.co>
2022-08-31 22:01:58 -0500

    includes map function parser

--- src/utils/init.js
+++ src/utils/init.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 let code = process.argv[2];
 
-let contentJs = `\nfunction _${code}_(lines) {\n\n\tlet [a, b] = lines.shift().split(' ');\n\n\treturn\n}\n\n//console.log(_${code}_(lines));\n\nmodule.exports = _${code}_;`;
+let contentJs = `\nfunction _${code}_(lines) {\n\n\tlet [a, b] = lines.shift().split(' ').map(e => parseFloat(e));\n\n\treturn\n}\n\n//console.log(_${code}_(lines));\n\nmodule.exports = _${code}_;`;
 let contentTest = `\nconst _${code}_ = require('../../problems/beginner/${code}');\n\ntest('${code} case 1 ', ()=>{\n    expect(_${code}_([''])).toBe('')\n});\n\ntest('${code} case 2 ', ()=>{\n    expect(_${code}_([''])).toBe('')\n});\n\ntest('${code} case 3 ', ()=>{\n    expect(_${code}_([''])).toBe('')\n});`;
 
 try {
