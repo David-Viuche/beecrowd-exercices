f78ebb70cb3a2b69e542077463ed9a5795019d79
David Viuche <jviucheroja@uniminuto.edu.co>
2022-10-12 19:04:37 -0500

    1052 Accepted

---
+++ src/problems/beginner/1052.js
@@ -0,0 +1,26 @@
+
+function _1052_(lines) {
+
+	let num = lines.shift();
+
+	let months = {
+		1: 'January',
+		2: 'February',
+		3: 'March',
+		4: 'April',
+		5: 'May',
+		6: 'June',
+		7: 'July',
+		8: 'August',
+		9: 'September',
+		10: 'October',
+		11: 'November',
+		12: 'December'
+	}
+
+	return months[num]
+}
+
+//console.log(_1052_(lines));
+
+module.exports = _1052_;
---
+++ src/test/beginner/1052.test.js
@@ -0,0 +1,6 @@
+
+const _1052_ = require('../../problems/beginner/1052');
+
+test('1052 case 1 ', ()=>{
+    expect(_1052_(['4'])).toBe('April')
+});
