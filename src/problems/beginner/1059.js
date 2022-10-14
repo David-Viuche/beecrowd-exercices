401d5645ae2fc90c6ea90eb3d2ed6f73888243af
David Viuche <jviucheroja@uniminuto.edu.co>
2022-10-14 18:35:44 -0500

    1059 Accepted

---
+++ src/problems/beginner/1059.js
@@ -0,0 +1,21 @@
+
+function _1059_() {
+
+	let num = 1;
+	
+	while (num <= 100){
+
+		let div = num/2
+
+		if (div.toString().includes('.')){
+			null;
+		}else{
+			console.log(num)
+		}
+
+		num = num + 1 	
+	}
+
+}
+
+_1059_()
