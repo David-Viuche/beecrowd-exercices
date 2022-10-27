2dbbc0a33af63e9f80006d5b7f1867aa83f2f352
David Viuche <jviucheroja@uniminuto.edu.co>
2022-10-26 19:01:41 -0500

    1060 Accepted fix

--- src/problems/beginner/1060.js
+++ src/problems/beginner/1060.js
@@ -4,7 +4,7 @@ function _1060_(lines) {
 	let numbers = lines.map(e => parseFloat(e))
 
 	let con = 0
-	
+
 	for (let i = 0; i < numbers.length; i++) {
 		if(numbers[i] > 0){
 			con++
@@ -15,6 +15,5 @@ function _1060_(lines) {
 }
 
 //console.log(_1060_(lines));
-console.log(_1060_(['7','-5','6','-3.4','4.6','12']));
 
 module.exports = _1060_;
